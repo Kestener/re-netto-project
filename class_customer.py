6867cb3bee619f9c0be089542624cec992fa9088
GerritKu <g.kuersch@gmail.com>
2023-03-01 15:28:43 +0100

    added probs and the locations as class variables to the customers class

--- class_customer.py
+++ class_customer.py
@@ -5,6 +5,15 @@ class Customer:
         self.customer_no = customer_no
         self.location = "entrance"
         self.active = True
+        self.probs = {"checkout" : [1, 0, 0, 0, 0, 0],
+                    "dairy" : [0.095, 0.599, 0.052, 0.104, 0.085,0.065],
+                    "drinks" : [0.139, 0.101, 0.455, 0.115, 0.095, 0.094],
+                    "entrance" : [0.169, 0.253, 0.153, 0.033, 0.265, 0.127],
+                    "fruit" : [0.137, 0.115, 0.078, 0.149, 0.466, 0.056],
+                    "spices" : [0.125, 0.190, 0.147, 0.138, 0.108, 0.292]
+                    }
+        # Can we reference this from the supermarket class?
+        self.possible_locations = ["checkout","dairy","drinks","entrance","fruit","spices"]
 
     def __repr__(self):
         return f'This is customer number {self.customer_no} at {self.location}'
@@ -31,16 +40,8 @@ class Customer:
     
     def move(self):
         if self.active:
-            possible_locations = ["checkout","dairy","drinks","entrance","fruit","spices"]
-            probs = {"checkout" : [1, 0, 0, 0, 0, 0],
-                    "dairy" : [0.095, 0.599, 0.052, 0.104, 0.085,0.065],
-                    "drinks" : [0.139, 0.101, 0.455, 0.115, 0.095, 0.094],
-                    "entrance" : [0.169, 0.253, 0.153, 0.033, 0.265, 0.127],
-                    "fruit" : [0.137, 0.115, 0.078, 0.149, 0.466, 0.056],
-                    "spices" : [0.125, 0.190, 0.147, 0.138, 0.108, 0.292]
-                    }
-            new_location = np.random.choice([0,1,2,3,4,5],p=probs[self.location])
-            new_location = possible_locations[new_location]
+            new_location = np.random.choice([0,1,2,3,4,5],p=self.probs[self.location])
+            new_location = self.possible_locations[new_location]
             self.location = new_location
             return new_location
         else:
