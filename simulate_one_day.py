aad90b02e93a59858a9d46393ea6beee28b05626
GerritKu <g.kuersch@gmail.com>
2023-03-02 18:17:18 +0100

    added two lines to call a plot_market class and create the visuals

--- simulate_one_day.py
+++ simulate_one_day.py
@@ -1,11 +1,23 @@
+#%%
 from supermarket_start import Supermarket
+from class_plot_market import PlotMarket
 
-re_netto = Supermarket(brand = 're-netto', open='07:00', closed = '22:00')
+#%%
+re_netto = Supermarket(brand = 're-netto_2', open='07:00', closed = '22:00')
 
 if __name__ == "__main__":
     
     while re_netto.is_open():
         re_netto.add_new_customers()
+        re_netto.print_customers(to_csv=True)        
         re_netto.next_minute()
-        re_netto.print_customers()
         re_netto.remove_exitsting_customers()
+
+#%%
+plot = PlotMarket(frames=1)
+plot.create_plots()
+plot.create_giph()
+# %%
+plot = PlotMarket(frames=1)
+plot.create_giph()
+# %%
