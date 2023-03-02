365b858125b497fb981aca8f2a82e68fbc4cc8e1
Kestener <r.kestener@gmail.com>
2023-03-02 17:42:51 +0100

    simulation order fixed

--- simulate_one_day.py
+++ simulate_one_day.py
@@ -8,6 +8,6 @@ Test_supermarket = Supermarket('Test_Supermarket')
 
 while Test_supermarket.is_open():
     Test_supermarket.add_new_customers()
-    Test_supermarket.next_minute()
     Test_supermarket.print_customers(to_csv=False)
+    Test_supermarket.next_minute()
     Test_supermarket.remove_exitsting_customers()
