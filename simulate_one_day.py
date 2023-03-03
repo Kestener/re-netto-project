f0d47927f22bd88bb4ad9963dd2c4fb3e3e8d288
Kestener <r.kestener@gmail.com>
2023-03-03 12:20:29 +0100

    simulate file fix

--- simulate_one_day.py
+++ simulate_one_day.py
@@ -5,11 +5,11 @@ Script to create a supermarket simulation
 from supermarket_start import Supermarket
 from class_plot_market import PlotMarket
 
-re_netto = Supermarket(brand = 're-netto_2', open='07:00', closed = '22:00')
+re_netto = Supermarket(brand = 're-netto_2', start='07:00', closed ='22:00')
 
-while Test_supermarket.is_open():
-    Test_supermarket.add_new_customers()
-    Test_supermarket.print_customers(to_csv=False)
-    Test_supermarket.next_minute()
-    Test_supermarket.remove_exitsting_customers()
+while re_netto.is_open():
+    re_netto.add_new_customers()
+    re_netto.print_customers(to_csv=False)
+    re_netto.next_minute()
+    re_netto.remove_exitsting_customers()
 
